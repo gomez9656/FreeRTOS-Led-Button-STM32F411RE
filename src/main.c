a89e7ea44f03b76208d2911a0a3013f6eb173efc
Juan Gomez <juanpg9656@gmail.com>
2021-03-17 09:51:59 -0500

    gpioa and gpioc initialization

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@
 
 
 static void prvSetupUart(void);
+static void prvSetupGPIO(void);
 static void prvSetupHardware(void);
 void printmsg(char *msg);
 char usr_msg[250];
@@ -75,49 +76,77 @@ void button_task_handler(void *params){
 
 static void prvSetupHardware(void){
 
+	//Setup Button and LED
+	prvSetupGPIO();
+
 	//Setup UART
 	prvSetupUart();
 
 }
 
+static void prvSetupGPIO(void){
+
+	GPIO_InitTypeDef led_init, button_init;
+
+	//Enable the GPIOA and GPIOC peripheral clock
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+	led_init.GPIO_Mode = GPIO_Mode_OUT;
+	led_init.GPIO_OType = GPIO_OType_PP;
+	led_init.GPIO_Pin = GPIO_Pin_5; //LED onboard
+	led_init.GPIO_Speed = GPIO_Low_Speed;
+	led_init.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+	GPIO_Init(GPIOA, &led_init);
+
+	button_init.GPIO_Mode = GPIO_Mode_IN;
+	button_init.GPIO_OType = GPIO_OType_PP;
+	button_init.GPIO_Pin = GPIO_Pin_13; //Button onboard
+	button_init.GPIO_Speed = GPIO_Low_Speed;
+	button_init.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+	GPIO_Init(GPIOC, &button_init);
+
+}
 
 static void prvSetupUart(void){
 
 	GPIO_InitTypeDef gpio_uart_pins;
-		USART_InitTypeDef uart2_init;
+	USART_InitTypeDef uart2_init;
 
-		//Enable the UART2 and GPIOA peripheral clock
-		RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
-		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	//Enable the UART2 and GPIOA peripheral clock
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2,ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
-		//PA2 is Tx. PA3 is Rx
-		//Alternate function configuration of MCU to behave as UART2
+	//PA2 is Tx. PA3 is Rx
+	//Alternate function configuration of MCU to behave as UART2
 
-		memset(&gpio_uart_pins, 0, sizeof(gpio_uart_pins));
+	memset(&gpio_uart_pins, 0, sizeof(gpio_uart_pins));
 
-		gpio_uart_pins.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
-		gpio_uart_pins.GPIO_Mode = GPIO_Mode_AF;
-		gpio_uart_pins.GPIO_PuPd = GPIO_PuPd_UP;
-		GPIO_Init(GPIOA, &gpio_uart_pins);
+	gpio_uart_pins.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+	gpio_uart_pins.GPIO_Mode = GPIO_Mode_AF;
+	gpio_uart_pins.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOA, &gpio_uart_pins);
 
-		//AF mode setting
-		GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2); //PA2
-		GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2); //PA3
+	//AF mode setting
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2); //PA2
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2); //PA3
 
-		//UART parameter initializations
+	//UART parameter initializations
 
-		memset(&uart2_init, 0, sizeof(uart2_init));
+	memset(&uart2_init, 0, sizeof(uart2_init));
 
-		uart2_init.USART_BaudRate = 115200;
-		uart2_init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-		uart2_init.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-		uart2_init.USART_Parity = USART_Parity_No;
-		uart2_init.USART_StopBits = USART_StopBits_1;
-		uart2_init.USART_WordLength = USART_WordLength_8b;
-		USART_Init(USART2, &uart2_init);
+	uart2_init.USART_BaudRate = 115200;
+	uart2_init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	uart2_init.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	uart2_init.USART_Parity = USART_Parity_No;
+	uart2_init.USART_StopBits = USART_StopBits_1;
+	uart2_init.USART_WordLength = USART_WordLength_8b;
+	USART_Init(USART2, &uart2_init);
 
-		//Enable the UART2 peripheral
-		USART_Cmd(USART2, ENABLE);
+	//Enable the UART2 peripheral
+	USART_Cmd(USART2, ENABLE);
 }
 
 void printmsg(char *msg){
